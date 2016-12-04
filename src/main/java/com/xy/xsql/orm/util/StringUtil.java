017102343e4cf8a4825c34dd1a6761d50e4ecef7
xiaoyao9184 <xiaoyao9184@gmail.com>
2016-12-04 11:06:12 +0800

    String join without null

--- src/main/java/com/xy/xsql/orm/util/StringUtil.java
+++ src/main/java/com/xy/xsql/orm/util/StringUtil.java
@@ -2,7 +2,9 @@ package com.xy.xsql.orm.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by xiaoyao9184 on 2016/11/24.
@@ -20,5 +22,21 @@ public class StringUtil {
         String [] fills = new String[count];
         Arrays.fill(fills, fill);
         return StringUtils.join(fills,separator);
+    }
+
+    /**
+     * Join if not NULL or Empty
+     * @param separator Separator
+     * @param strings String Array
+     * @return String
+     */
+    public static String join(String separator,String... strings){
+        List<String> stringList = new ArrayList<>();
+        for (String str: strings) {
+            if(!CheckUtil.isNullOrEmpty(str)){
+                stringList.add(str);
+            }
+        }
+        return StringUtils.join(stringList,separator);
     }
 }
