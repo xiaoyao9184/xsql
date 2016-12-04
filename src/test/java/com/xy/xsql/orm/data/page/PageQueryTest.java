6939564abbd711fc827dbee629654631462d20aa
xiaoyao9184 <xiaoyao9184@gmail.com>
2016-12-04 10:07:19 +0800

    PageQuery unit test

---
+++ src/test/java/com/xy/xsql/orm/data/page/PageQueryTest.java
@@ -0,0 +1,66 @@
+package com.xy.xsql.orm.data.page;
+
+import org.junit.Test;
+
+/**
+ * Created by xiaoyao9184 on 2016/11/2.
+ */
+public class PageQueryTest {
+
+    @Test
+    public void TestStartWithZero(){
+        //来源：从0开始，第0页
+        PageQuery page = new PageQuery()
+                .withStartWithZero(true)
+                .withPageNo(0)
+                .withPageSize(10);
+
+        assert page.getPageStart() == 0;
+        //目标：从0开始，第0页
+        assert page.getPageStartWithZero() == 0;
+        //目标：从1开始，第1页
+        assert page.getPageStartWithOne() == 1;
+
+
+        //来源：从0开始，第1页
+        page = new PageQuery()
+                .withStartWithZero(true)
+                .withPageNo(1)
+                .withPageSize(10);
+
+        assert page.getPageStart() == 1;
+        //目标：从0开始，第1页
+        assert page.getPageStartWithZero() == 1;
+        //目标：从1开始，第2页
+        assert page.getPageStartWithOne() == 2;
+    }
+
+    @Test
+    public void TestStartWithOne(){
+        //来源：从1开始，第1页
+        PageQuery page = new PageQuery()
+                .withStartWithZero(false)
+                .withPageNo(1)
+                .withPageSize(10);
+
+        assert page.getPageStart() == 1;
+        //目标：从0开始，第0页
+        assert page.getPageStartWithZero() == 0;
+        //目标：从1开始，第1页
+        assert page.getPageStartWithOne() == 1;
+
+
+        //来源：从1开始，第2页
+        page = new PageQuery()
+                .withStartWithZero(false)
+                .withPageNo(2)
+                .withPageSize(10);
+
+        assert page.getPageStart() == 2;
+        //目标：从0开始，第1页
+        assert page.getPageStartWithZero() == 1;
+        //目标：从1开始，第2页
+        assert page.getPageStartWithOne() == 2;
+    }
+
+}
