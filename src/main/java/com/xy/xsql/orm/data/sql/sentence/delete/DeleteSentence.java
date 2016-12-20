7ea06910bf4dd85fd6d0bdf95bef02684565049a
xiaoyao9184 <xiaoyao9184@gmail.com>
2016-12-20 20:26:40 +0800

    Use MSDN delete sentence syntax

--- src/main/java/com/xy/xsql/orm/data/sql/sentence/delete/DeleteSentence.java
+++ src/main/java/com/xy/xsql/orm/data/sql/sentence/delete/DeleteSentence.java
@@ -7,28 +7,68 @@ import com.xy.xsql.orm.data.sql.element.OtherEnum;
 import com.xy.xsql.orm.data.sql.element.info.Table;
 import com.xy.xsql.orm.data.sql.sentence.BaseElementsSentence;
 import com.xy.xsql.orm.data.sql.sentence.CustomizeSentence;
-import com.xy.xsql.orm.data.sql.sentence.select.JoinItem;
+import com.xy.xsql.orm.data.sql.sentence.select.TopItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * https://msdn.microsoft.com/en-us/library/ms189835.aspx
+ *
+ -- Syntax for SQL Server and Azure SQL Database
+
+ [ WITH <common_table_expression> [ ,...n ] ]
+ DELETE
+ [ TOP ( expression ) [ PERCENT ] ]
+ [ FROM ]
+ { { table_alias
+ | <object>
+ | rowset_function_limited
+ [ WITH ( table_hint_limited [ ...n ] ) ] }
+ | @table_variable
+ }
+ [ <OUTPUT Clause> ]
+ [ FROM table_source [ ,...n ] ]
+ [ WHERE { <search_condition>
+ | { [ CURRENT OF
+ { { [ GLOBAL ] cursor_name }
+ | cursor_variable_name
+ }
+ ]
+ }
+ }
+ ]
+ [ OPTION ( <Query Hint> [ ,...n ] ) ]
+ [; ]
+
+ <object> ::=
+ {
+ [ server_name.database_name.schema_name.
+ | database_name. [ schema_name ] .
+ | schema_name.
+ ]
+ table_or_view_name
+ }
  * Created by xiaoyao9184 on 2016/10/15.
  */
 public class DeleteSentence extends CustomizeSentence {
-
-    private List<Table> tables;
+    //TOP
+    private TopItem top;
+    //
     private Table table;
-    private List<JoinItem> joins;
+    //FROM table_source
+    private List<Table> tables;
+    //WHERE
     private Expression where;
-//    private Limit limit;
-//    private List<OrderByElement> orderByElements;
 
 
     public DeleteSentence() {
         super();
     }
 
+    public void withTop(TopItem top){
+        this.top = top;
+    }
 
     public void withTable(Table table) {
         if(this.tables == null){
@@ -41,103 +81,72 @@ public class DeleteSentence extends CustomizeSentence {
         this.table = table;
     }
 
-    public void withJoin(JoinItem join) {
-        if(this.joins == null){
-            this.joins = new ArrayList<>();
-        }
-        this.joins.add(join);
+    public void withWhere(Expression where) {
+        this.where = where;
     }
-
 
     @Override
     public BaseElementsSentence toBaseElementsSentence() {
         ListElementBuilder builder = new ListElementBuilder()
                 .append(GrammarEnum.DELETE);
+
+        //[ TOP ( expression ) [ PERCENT ] ]
+        builder.append(OtherEnum.SPACE)
+                .append(top.toElementList(),null);
+
+        //[ FROM ]
+        builder.append(OtherEnum.SPACE)
+                .append(GrammarEnum.FROM);
+
+        /*
+        { { table_alias
+          | <object>
+          | rowset_function_limited
+          [ WITH ( table_hint_limited [ ...n ] ) ] }
+          | @table_variable
+        }
+         */
+        builder.append(OtherEnum.SPACE)
+                .append(table.getAliasName())
+                .append(OtherEnum.SPACE);
+
+        //[ FROM table_source [ ,...n ] ]
         if(tables != null && tables.size() > 0){
-            builder.append(OtherEnum.SPACE);
+            builder.append(OtherEnum.SPACE)
+                    .append(GrammarEnum.FROM);
             int i = 0;
             for(Table t : tables){
                 if(i != 0){
                     builder.append(OtherEnum.DELIMITER);
                 }
-                builder.append(t.getAliasName());
+                builder.append(t.getFullName());
                 i++;
             }
         }
 
-        builder.append(OtherEnum.SPACE);
-        builder.append(GrammarEnum.FROM);
-        builder.append(OtherEnum.SPACE);
-        builder.append(table.getFullName());
-        builder.append(OtherEnum.SPACE);
-        if (joins != null) {
-            for (JoinItem join : joins) {
-                if (join.isSimple()) {
-                    builder.append(OtherEnum.DELIMITER)
-                            .append(join.toElementList(),OtherEnum.SPACE);
-                } else {
-                    builder.append(OtherEnum.SPACE)
-                            .append(join.toElementList(),OtherEnum.SPACE);
+        /*
+        [ WHERE { <search_condition>
+                | { [ CURRENT OF
+                       { { [ GLOBAL ] cursor_name }
+                           | cursor_variable_name
+                       }
+                    ]
+                  }
                 }
-            }
-        }
-
+        ]
+        */
         if( where != null ){
             builder.append(OtherEnum.SPACE)
                     .append(GrammarEnum.WHERE)
                     .append(OtherEnum.SPACE)
                     .append(where);
         }
-
 
         return new BaseElementsSentence(builder.build(null));
     }
 
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder()
-                .append(GrammarEnum.DELETE);
-        if(tables != null && tables.size() > 0){
-            builder.append(OtherEnum.SPACE);
-            int i = 0;
-            for(Table t : tables){
-                if(i != 0){
-                    builder.append(OtherEnum.DELIMITER);
-                }
-                builder.append(t.getAliasName());
-                i++;
-            }
-        }
-
-        builder.append(OtherEnum.SPACE);
-        builder.append(GrammarEnum.FROM);
-        builder.append(OtherEnum.SPACE);
-        builder.append(table.getFullName());
-        builder.append(OtherEnum.SPACE);
-        if (joins != null) {
-            for (JoinItem join : joins) {
-                if (join.isSimple()) {
-                    builder.append(OtherEnum.DELIMITER).append(join);
-                } else {
-                    builder.append(OtherEnum.SPACE).append(join);
-                }
-            }
-        }
-
-        if( where != null ){
-            builder.append(OtherEnum.SPACE)
-                    .append(GrammarEnum.WHERE)
-                    .append(OtherEnum.SPACE)
-                    .append(where);
-        }
-
-//        if(orderByElements!=null){
-//            b.append(PlainSelect.orderByToString(orderByElements));
-//        }
-//
-//        if(limit != null){
-//            b.append(limit);
-//        }
-        return builder.toString();
+        return toBaseElementsSentence().toString();
     }
 }
