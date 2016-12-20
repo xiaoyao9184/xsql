d810dc5361c8adb71f8ead8c0e507862e3aa674b
xiaoyao9184 <xiaoyao9184@gmail.com>
2016-12-20 20:27:09 +0800

    Use MSDN insert sentence syntax

--- src/main/java/com/xy/xsql/orm/data/sql/sentence/insert/InsertSentence.java
+++ src/main/java/com/xy/xsql/orm/data/sql/sentence/insert/InsertSentence.java
@@ -9,22 +9,67 @@ import com.xy.xsql.orm.data.sql.element.info.Table;
 import com.xy.xsql.orm.data.sql.expression.operator.relational.Group;
 import com.xy.xsql.orm.data.sql.sentence.BaseElementsSentence;
 import com.xy.xsql.orm.data.sql.sentence.CustomizeSentence;
+import com.xy.xsql.orm.data.sql.sentence.select.TopItem;
+import com.xy.xsql.orm.util.CheckUtil;
 import com.xy.xsql.orm.util.ListUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * https://msdn.microsoft.com/en-us/library/ms174335.aspx
+ *
+ -- Syntax for SQL Server and Azure SQL Database
+
+ [ WITH <common_table_expression> [ ,...n ] ]
+ INSERT
+ {
+ [ TOP ( expression ) [ PERCENT ] ]
+ [ INTO ]
+ { <object> | rowset_function_limited
+ [ WITH ( <Table_Hint_Limited> [ ...n ] ) ]
+ }
+ {
+ [ ( column_list ) ]
+ [ <OUTPUT Clause> ]
+ { VALUES ( { DEFAULT | NULL | expression } [ ,...n ] ) [ ,...n     ]
+ | derived_table
+ | execute_statement
+ | <dml_table_source>
+ | DEFAULT VALUES
+ }
+ }
+ }
+ [;]
+
+ <object> ::=
+ {
+ [ server_name . database_name . schema_name .
+ | database_name .[ schema_name ] .
+ | schema_name .
+ ]
+ table_or_view_name
+ }
+
+ <dml_table_source> ::=
+ SELECT <select_list>
+ FROM ( <dml_statement_with_output_clause> )
+ [AS] table_alias [ ( column_alias [ ,...n ] ) ]
+ [ WHERE <search_condition> ]
+ [ OPTION ( <query_hint> [ ,...n ] ) ]
+
  * Created by xiaoyao9184 on 2016/10/15.
  */
 public class InsertSentence extends CustomizeSentence {
 
+    //TOP
+    private TopItem top;
+    private boolean useInto;
     private Table table;
-
+    //
     private List<Column> columns;
-
+    //{ VALUES ( { DEFAULT | NULL | expression } [ ,...n ] ) [ ,...n     ]
     private boolean useValues = true;
-
     private List<Group> groupValues;
 
     public InsertSentence() {
@@ -91,46 +136,39 @@ public class InsertSentence extends CustomizeSentence {
     @Override
     public BaseElementsSentence toBaseElementsSentence() {
         ListElementBuilder builder = new ListElementBuilder()
-                .append(GrammarEnum.INSERT)
-                .append(OtherEnum.SPACE)
-                .append(GrammarEnum.INTO)
-                .append(OtherEnum.SPACE)
-                .append(table)
-                .append(OtherEnum.GROUP_START);
-        for (Column column: columns) {
-            builder.append(column);
-            builder.append(OtherEnum.DELIMITER);
+                .append(GrammarEnum.INSERT);
+
+        //[ TOP ( expression ) [ PERCENT ] ]
+        builder.append(OtherEnum.SPACE)
+                .append(top.toElementList(),null);
+
+        //[ INTO ]
+        if(useInto){
+            builder.append(OtherEnum.SPACE)
+                    .append(GrammarEnum.INTO);
         }
-        builder
-                .append(OtherEnum.GROUP_END)
-                .append(useValues ? GrammarEnum.VALUES : null);
+
+        //
+        builder.append(OtherEnum.SPACE)
+                .append(table);
+
+        //[ ( column_list ) ]
+        if(!CheckUtil.isNullOrEmpty(columns)){
+            builder.append(OtherEnum.GROUP_START);
+            for (Column column: columns) {
+                builder.append(column);
+                builder.append(OtherEnum.DELIMITER);
+            }
+            builder.append(OtherEnum.GROUP_END);
+        }
+        //{ VALUES ( { DEFAULT | NULL | expression } [ ,...n ] ) [ ,...n     ]
+        builder.append(useValues ? GrammarEnum.VALUES : null);
         for (Group group: groupValues) {
             builder.append(group.toElementList(),OtherEnum.DELIMITER);
             builder.append(OtherEnum.SPACE);
         }
+
         return new BaseElementsSentence(builder.build(null));
     }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder()
-                .append(GrammarEnum.INSERT)
-                .append(OtherEnum.SPACE)
-                .append(GrammarEnum.INTO)
-                .append(OtherEnum.SPACE)
-                .append(table.getFullName())
-                .append(OtherEnum.SPACE)
-                .append(OtherEnum.GROUP_START);
-        for (Column column: columns) {
-            builder.append(column);
-            builder.append(OtherEnum.DELIMITER);
-        }
-        builder.append(OtherEnum.GROUP_END)
-                .append(useValues ? GrammarEnum.VALUES : "");
-        for (Group group: groupValues) {
-            builder.append(group);
-            builder.append(OtherEnum.SPACE);
-        }
-        return builder.toString();
-    }
 }
