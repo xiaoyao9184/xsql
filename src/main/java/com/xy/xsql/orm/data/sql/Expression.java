b06dda8fbd549822742b6bed3d2b3b555a107ac1
xiaoyao9184 <xiaoyao9184@gmail.com>
2016-12-22 19:29:18 +0800

    ElementList

--- src/main/java/com/xy/xsql/orm/data/sql/Expression.java
+++ src/main/java/com/xy/xsql/orm/data/sql/Expression.java
@@ -1,15 +1,9 @@
 package com.xy.xsql.orm.data.sql;
 
-import java.util.List;
-
 /**
  * Expression
  * Created by xiaoyao9184 on 2016/10/22.
  */
-public interface Expression extends Element {
-    /**
-     * to elements
-     * @return elements
-     */
-    List<Element> toElementList();
+public interface Expression extends ElementList {
+
 }
