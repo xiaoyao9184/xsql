4163fe049be21b201f624bc7e710b15f70b2ece9
xiaoyao9184 <xiaoyao9184@gmail.com>
2017-01-13 17:15:18 +0800

    Set Operators

--- src/main/java/com/xy/xsql/orm/data/sql/element/OperatorEnum.java
+++ src/main/java/com/xy/xsql/orm/data/sql/element/OperatorEnum.java
@@ -6,18 +6,39 @@ import com.xy.xsql.orm.data.sql.Element;
  * Created by xiaoyao9184 on 2016/10/22.
  */
 public enum OperatorEnum implements Element {
+
+    /**
+     * Arithmetic Operators
+     * https://msdn.microsoft.com/en-us/library/ms187716.aspx
+     */
     ADDITION("+",Type.Arithmetic),
     SUBTRACTION("-",Type.Arithmetic),
     MULTIPLICATION("*",Type.Arithmetic),
     DIVISION("/",Type.Arithmetic),
     MODULO("%",Type.Arithmetic),
 
+    /**
+     * Assignment Operator
+     * https://msdn.microsoft.com/en-us/library/ms188343.aspx
+     */
     Assignment("=",Type.Assignment),
 
+    /**
+     * Bitwise Operators
+     * https://msdn.microsoft.com/en-us/library/ms176122.aspx
+     */
     BITWISE_AND("&",Type.Bitwise),
+    BITWISE_AND_EQUALS("&=",Type.Bitwise),
     BITWISE_OR("|",Type.Bitwise),
+    BITWISE_OR_EQUALS("|=",Type.Bitwise),
     BITWISE_XOR("^",Type.Bitwise),
+    BITWISE_XOR_EQUALS("^=",Type.Bitwise),
+    BITWISE_NOT("~",Type.Bitwise),
 
+    /**
+     * Comparison Operators
+     * https://msdn.microsoft.com/en-us/library/ms188074.aspx
+     */
     EQUAL("=",Type.Comparison),
     GREATER(">",Type.Comparison),
     LESS("<",Type.Comparison),
@@ -28,6 +49,10 @@ public enum OperatorEnum implements Element {
     NOT_LESS_NOT_ISO("!<",Type.Comparison),
     NOT_GREATER_NOT_ISO("!>",Type.Comparison),
 
+    /**
+     * Compound Operators
+     * https://msdn.microsoft.com/en-us/library/cc645922.aspx
+     */
     Add_EQUALS("+=",Type.Compound),
     Subtract_EQUALS("-=",Type.Compound),
     Multiply_EQUALS("*=",Type.Compound),
@@ -37,6 +62,10 @@ public enum OperatorEnum implements Element {
     Bitwise_Exclusive_OR_EQUALS("^=",Type.Compound),
     Bitwise_OR_EQUALS("|=",Type.Compound),
 
+    /**
+     * Logical Operators
+     * https://msdn.microsoft.com/en-us/library/ms189773.aspx
+     */
     ALL(Type.Logical),
     AND(Type.Logical),
     ANY(Type.Logical),
@@ -48,19 +77,44 @@ public enum OperatorEnum implements Element {
     OR(Type.Logical),
     SOME(Type.Logical),
 
-    //TODO
-    ILIKE("ILIKE"),
-    //TODO
-    CONCAT("||"),
-
+    /**
+     * Scope Resolution Operator
+     * https://msdn.microsoft.com/en-us/library/dd206995.aspx
+     */
     Scope_Resolution("::",Type.Scope_Resolution),
 
+    /**
+     * Set Operators
+     * https://msdn.microsoft.com/en-us/library/ff848745.aspx
+     */
+    EXCEPT("EXCEPT",Type.Set),
+    INTERSECT("INTERSECT",Type.Set),
+    UNION("UNION",Type.Set),
+    UNION_ALL("UNION ALL",Type.Set),
+
+    /**
+     * String Operators
+     * https://msdn.microsoft.com/en-us/library/ms190301.aspx
+     */
     String_Concatenation("+",Type.String),
     String_Concatenation_Set("+=",Type.String),
 
+    /**
+     * Unary Operators
+     * https://msdn.microsoft.com/en-us/library/ms188400.aspx
+     */
     Positive("+",Type.Unary),
     Negative("-",Type.Unary),
-    Bitwise_NOT("~",Type.Unary);
+    Bitwise_NOT("~",Type.Unary),
+
+
+
+    @Deprecated
+    ILIKE("ILIKE"),
+
+    @Deprecated
+    CONCAT("||");
+
 
     private String string;
     private Type type;
@@ -96,6 +150,7 @@ public enum OperatorEnum implements Element {
         Scope_Resolution,
         Bitwise,
         Comparison,
+        Set,
         String,
         Compound,
         Unary
