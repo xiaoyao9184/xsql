f48306804b0a17a2a5c4c89c508647ae8ea47214
xiaoyao9184 <xiaoyao9184@gmail.com>
2017-06-21 15:25:47 +0800

    IntoBuilder

---
+++ tsql-core/src/main/java/com/xy/xsql/tsql/core/clause/select/IntoBuilder.java
@@ -0,0 +1,33 @@
+package com.xy.xsql.tsql.core.clause.select;
+
+import com.xy.xsql.core.builder.CodeTreeBuilder;
+import com.xy.xsql.tsql.model.clause.select.Into;
+import com.xy.xsql.tsql.model.datatype.StringConstant;
+import com.xy.xsql.tsql.model.element.TableName;
+
+/**
+ * IntoBuilder
+ * Created by xiaoyao9184 on 2017/6/21.
+ */
+public class IntoBuilder<ParentBuilder>
+        extends CodeTreeBuilder<IntoBuilder<ParentBuilder>,ParentBuilder,Into> {
+
+    public IntoBuilder() {
+        super(new Into());
+    }
+
+    public IntoBuilder(Into into) {
+        super(into);
+    }
+
+    public IntoBuilder<ParentBuilder> withNewTable(TableName tableName){
+        target.setNewTable(tableName);
+        return this;
+    }
+
+    public IntoBuilder<ParentBuilder> withFileGroup(String fileGroup){
+        target.setFileGroup(new StringConstant(fileGroup));
+        return this;
+    }
+
+}
