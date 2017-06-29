b770a3f16dafe32aa4cd4b7af0eff3092c4f2851
xiaoyao9184 <xiaoyao9184@gmail.com>
2017-06-29 12:09:13 +0800

    QUERY method for QuerySpecificationBuilder

--- tsql-core/src/main/java/com/xy/xsql/tsql/core/clause/subquery/SubQueryBuilder.java
+++ tsql-core/src/main/java/com/xy/xsql/tsql/core/clause/subquery/SubQueryBuilder.java
@@ -1,24 +1,13 @@
 package com.xy.xsql.tsql.core.clause.subquery;
 
-import com.xy.xsql.core.builder.SubBuilder;
-import com.xy.xsql.tsql.core.clause.TableValueConstructorBuilder;
+import com.xy.xsql.tsql.core.statement.dml.SelectBuilder;
 
 /**
  * Created by xiaoyao9184 on 2017/3/10.
  */
-public class SubQueryBuilder<Done>
-        extends SubBuilder<SubQueryBuilder<Done>,Void,Done> {
+public class SubQueryBuilder {
 
-    public SelectSubQueryBuilder<SubQueryBuilder<Done>> select() {
-        return new SelectSubQueryBuilder<SubQueryBuilder<Done>>().in(this);
-    }
-
-    public TableValueConstructorBuilder<SubQueryBuilder<Done>> values() {
-        return new TableValueConstructorBuilder<SubQueryBuilder<Done>>().in(this);
-    }
-
-    public SubQueryBuilder<Done> withAs(){
-
-        return this;
+    public static SelectBuilder.QuerySpecificationBuilder<Void> QUERY(){
+        return new SelectBuilder.QuerySpecificationBuilder<>();
     }
 }
