a905e6aab0c0fe54111abc13ec861b7293e3c0af
xiaoyao9184 <xiaoyao9184@gmail.com>
2018-06-01 13:24:27 +0800

    ScalarExpression missing extend

--- tsql-model/src/main/java/com/xy/xsql/tsql/model/elements/expressions/ScalarExpression.java
+++ tsql-model/src/main/java/com/xy/xsql/tsql/model/elements/expressions/ScalarExpression.java
@@ -3,6 +3,6 @@ package com.xy.xsql.tsql.model.elements.expressions;
 /**
  * Created by xiaoyao9184 on 2017/6/29.
  */
-public interface ScalarExpression {
+public interface ScalarExpression extends Expression {
 
 }
